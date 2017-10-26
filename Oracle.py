ef42c088f07112f3b082f7bbb2901b55129c4e12
harper-carroll <harpercarroll@gmail.com>
2017-10-25 22:39:19 -0700

    Update Oracle.py

--- Oracle.py
+++ Oracle.py
@@ -7,9 +7,9 @@ Class Oracle():
 	def __init__(self):
 		self.actions = ["Buy", "Sell", "Hold"]
 
-    def getAction(self, date, first_day, last_day): #first_day = min(d, key=d.get), last_day = max(d, key=d.get)
-    	if date == first_day: return "Buy"
-    	elif date == last_day: return "Sell"
-    	else: return "Hold"
+	def getAction(self, date, first_day, last_day): #first_day = min(d, key=d.get), last_day = max(d, key=d.get)
+    		if date == first_day: return "Buy"
+    		elif date == last_day: return "Sell"
+    		else: return "Hold"
 
 
